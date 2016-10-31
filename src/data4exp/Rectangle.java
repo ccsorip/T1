3a8ccf7c8db266afac0941c3d583a254e949ee83
desarrollo <desarrollo@qhawayra.dgf.uchile.cl>
2016-10-31 19:47:44 -0300

    Rectangulos sintéticos...

---
+++ src/data4exp/Rectangle.java
@@ -0,0 +1,13 @@
+package data4exp;
+
+public abstract class Rectangle {
+
+
+	protected float x1,y1,x2,y2;
+	
+
+	public abstract String toString();
+	public abstract float width();
+	public abstract float height();
+	public abstract float [] coords();
+}
---
+++ src/data4exp/SynthRectangleGen.java
@@ -0,0 +1,14 @@
+package data4exp;
+
+public class SynthRectangleGen {
+
+	private int n;
+	public SynthRectangleGen(){
+		this.n=0;
+	}
+	public SynthRectangleGen(int n){
+		this.n=n;
+	}
+	
+
+}
