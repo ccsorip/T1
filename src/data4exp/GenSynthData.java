e1e5f6116a61b7295556ceea4a3bf34c331d97be
desarrollo <desarrollo@qhawayra.dgf.uchile.cl>
2016-10-31 22:33:46 -0300

    GenSynthData debería escribir los datos en un TXT

---
+++ src/data4exp/GenSynthData.java
@@ -0,0 +1,35 @@
+package data4exp;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class GenSynthData {
+
+	public GenSynthData(){}
+	
+	public static void genFile(int n){
+		
+		int n_exp=(int)Math.pow(2,n);
+		try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+	              new FileOutputStream("experiment_"+Integer.toString(n)+".txt"), "utf-8"))){ 
+			while(n_exp!=0){
+				Rectangle rect = new SynthRectangle();
+				writer.write(rect.toString());
+				n_exp--;
+				System.out.println(n_exp);
+			}}catch(IOException e){}
+		
+		}
+	
+	public void genFiles(int ini, int end){
+		
+	}
+	
+	public static void main(String [] args){
+		genFile(2);
+		System.out.println("Holaaa");
+	}
+}
