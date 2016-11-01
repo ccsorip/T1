85ff989dc9116b55aca564e05bf12f82e9c560b4
ccsorip <coni.csorip@gmail.com>
2016-10-31 22:35:23 -0300

    falta pickseeds y picknext

--- src/tree/Linear.java
+++ src/tree/Linear.java
@@ -1,9 +1,20 @@
 package tree;
 
+import java.util.LinkedList;
+
+import node.*;
+
 public class Linear extends RTree{
 
 	public Linear(int blockSize) {
 		super(blockSize);
 	}
+	
+	protected Entry[] pickSeeds(LinkedList<Entry> children){
+		return null;
+	}
 
+	protected Entry pickNext(LinkedList<Entry> children){
+		return null;
+	}
 }
