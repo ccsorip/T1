905741111e5d2876c7232821c01145019654e9ca
Paloma <paloma.perez.gar@gmail.com>
2016-11-08 14:37:23 -0300

    Prueba de rectangulos sintéticos....

--- src/data4exp/SynthRectangle.java
+++ src/data4exp/SynthRectangle.java
@@ -2,16 +2,17 @@ package data4exp;
 
 import java.util.Random;
 
-public class SynthRectangle extends Rectangle {
+public class SynthRectangle implements Rectangle {
 
 	private float min_val=0.0f;
 	private float max_val=500000.0f;
+	protected float x1_,y1_,x2_,y2_;
 	
 	public SynthRectangle(){
 
 		float x1,y1,x2,y2;
 		Random rand = new Random();
-		x1=x2=y1=y2=0;
+		x1=x2=y1=y2=0.0f;
 		
 		while(Math.abs(x2-x1)>100 || Math.abs(x2-x1)==0){
 
@@ -21,44 +22,52 @@ public class SynthRectangle extends Rectangle {
 		
 		while(Math.abs(y2-y1)>100 || Math.abs(y2-y1)==0){
 
-			x1= rand.nextFloat() * (max_val - min_val) + min_val;
-			x2= rand.nextFloat() * (max_val - min_val) + min_val;
+			y1= rand.nextFloat() * (max_val - min_val) + min_val;
+			y2= rand.nextFloat() * (max_val - min_val) + min_val;
 		}
 		
-		this.x1=x1; this.x2=x2; this.y1=y1; this.y2=y2;
+
+		this.x1_=x1<x2?x1:x2;
+		this.x2_=x1>x2?x1:x2;
+		this.y1_=y1<y2?y1:y2; 
+		this.y2_=y1>y2?y1:y2;
+		
 	}
+	
 
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
 		StringBuilder sb = new StringBuilder(); 
-		sb.append(x1);
+		sb.append(x1_);
 		sb.append(',');
-		sb.append(y1);
+		sb.append(y1_);
 		sb.append(',');
-		sb.append(x2);
+		sb.append(x2_);
 		sb.append(',');
-		sb.append(y2);
+		sb.append(y2_);
 		sb.append('\n');
+		
 		return sb.toString();
 	}
 
 	@Override
 	public float width() {
 		// TODO Auto-generated method stub
-		return Math.abs(this.x2-this.x1);
+		return Math.abs(this.x2_-this.x1_);
 	}
 
 	@Override
 	public float height() {
 		// TODO Auto-generated method stub
-		return Math.abs(this.y2-this.y1);
+		return Math.abs(y2_-y1_);
 	}
 
+	
 	@Override
 	public float[] coords() {
 		// TODO Auto-generated method stub
-		float [] coords = {this.x1, this.y1, this.x2, this.y2};
+		float [] coords = {x1_, y1_, x2_, y2_};
 		return coords;
 	}
 }
